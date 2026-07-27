e4f55b5f9ebc84bce3262902c975093e4a48ba5c
mosfeqanik <mosfeqanik01@gmail.com>
2026-07-27 22:07:23 +0600

    Initial commit

---
+++ src/main/java/tech/anik/employemanager/EmployemanagerApplication.java
@@ -0,0 +1,13 @@
+package tech.anik.employemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/anik/employemanager/EmployemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.anik.employemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
